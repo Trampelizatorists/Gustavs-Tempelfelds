7e0cbbdd01d691299fbb8e97c81a30c6bbdbf85f
skolens <johndoe@gmail.com>
2024-10-18 22:05:21 +1300

    w

--- uzd2.py
+++ uzd2.py
@@ -4,7 +4,7 @@ loggedin = False
 while loggedin == False:
     ievads = str(input("Ievadi paroli: "))
     if ievads == correct_password:
-        print("Tu esi ielogojies")
+        print("Piekļuve atļauta!")
         loggedin = True
     elif ievads != correct_password:
-        print("Parole nesakrīt")
+        print("Piekļuve liegta")
